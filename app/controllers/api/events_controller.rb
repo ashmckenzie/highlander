daac69932ca24eece452d18060adf4b7a431395e
Ash McKenzie <ash@greenworm.com.au>
2013-02-28 16:59:52 +1100

    woops

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -2,7 +2,7 @@ class Api::EventsController < ApiController
 
   def create
 
-    User.where(email: params['email']).events << Event.create(:)
+    # User.where(email: params['email']).events << Event.create()
 
     respond_to do |format|
       format.html
