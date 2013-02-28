14ddddca23ab523766eb4b6d805c4329b4894698
Ash McKenzie <ash@greenworm.com.au>
2013-02-28 14:16:01 +1100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Highlander::Application.config.secret_key_base = 'fb60bfb1b83e5022f5823e1995f290d2ca78927faeabc4ab89282319a171068d9d340e90bea28b2e054868c8d206e2ca86d9fdfa9cf95e125697aca123249a67'
