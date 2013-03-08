a5e6762ad60e8e5c4b873a05de604e7416bc48f2
Ash McKenzie <ash@greenworm.com.au>
2013-03-08 12:03:50 +1100

    Handle unknown metrics

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,11 +1,15 @@
 class Api::EventsController < ApiController
 
   def create
-    current_user.events << Event.create(user: current_user, metric: Metric.where(name: params['metric']).first)
+    if metric = Metric.where(name: params['metric']).first
+      current_user.events << Event.create(user: current_user, metric: metric)
+      code = :ok
+    else
+      code = :not_found
+    end
 
     respond_to do |format|
-      format.html
-      format.json { head :ok }
+      format.json { head code }
     end
   end
 end
