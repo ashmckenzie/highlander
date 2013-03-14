455e2a384fc5bfd44ea803a3b352bc795179107a
Ash McKenzie <ash@greenworm.com.au>
2013-03-14 17:43:56 +1100

    GitHub adapter

---
+++ app/controllers/api/adapters/github_controller.rb
@@ -0,0 +1,27 @@
+class Api::Adapters::GithubController < Api::AdapterController
+
+  def create
+    code = :ok
+    current_user.events << Event.create(user: current_user, metric: metric)
+
+    respond_to do |format|
+      format.json { head code }
+    end
+  end
+
+  private
+
+  def email_address
+    params[:commits].first[:author][:email]
+  end
+
+  def metric
+    Metric.where(name: 'github_push').first
+  end
+
+  def current_user
+    @current_user ||= User.where(email: email_address).first
+  rescue
+    raise InvalidUserToken
+  end
+end
