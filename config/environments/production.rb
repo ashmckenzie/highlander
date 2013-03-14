1d267e77dcd1f1dba53a4d45c94ece6394afcb9b
Stuart Liston <stuart.liston@gmail.com>
2013-03-14 16:20:24 +1100

    hopefully fixing assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,10 +27,13 @@ Highlander::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Whether to fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
+
+  # Compress JavaScripts and CSS
+  config.assets.compress = true
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
