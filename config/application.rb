ebed8318031295896881ada04e9ba64c080ef480
Ash McKenzie <ash@greenworm.com.au>
2013-03-15 09:58:42 +1100

    Desperate

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,14 @@ require "action_mailer/railtie"
 require "sprockets/railtie"
 
 # Assets should be precompiled for production (so we don't need the gems loaded then)
-Bundler.require(*Rails.groups(assets: %w(development test)))
+# Bundler.require(*Rails.groups(assets: %w(development test)))
+
+if defined?(Bundler)
+  # If you precompile assets before deploying to production, use this line
+  # Bundler.require *Rails.groups(:assets => %w(development test))
+  # If you want your assets lazily compiled in production, use this line
+  Bundler.require(:default, :assets, Rails.env)
+end
 
 module Highlander
   class Application < Rails::Application
