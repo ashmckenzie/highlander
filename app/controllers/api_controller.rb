aa44da2e47ca1f44ed7d7558b21c74b1fc5b3b61
Ash McKenzie <ash@greenworm.com.au>
2013-03-15 21:01:18 +1100

    Argh, what's the REAL IP Heroku

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -61,6 +61,7 @@ class ApiController < ApplicationController
   end
 
   def request_ip_address
+    Rails.logger.info env.inspect
     IPAddr.new(env['HTTP_X_REAL_IP'] || env['REMOTE_ADDR'])
   end
 
