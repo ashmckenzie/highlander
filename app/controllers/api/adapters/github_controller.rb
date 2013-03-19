128f1a26371039ea0c2260672de21fd3e244431d
Ash McKenzie <ash@greenworm.com.au>
2013-03-19 20:43:21 +1100

    Ensure points only awarded for GH pushes to master

--- app/controllers/api/adapters/github_controller.rb
+++ app/controllers/api/adapters/github_controller.rb
@@ -1,6 +1,16 @@
 class Api::Adapters::GithubController < Api::AdapterController
 
-  before_filter :validate_request_source
+  MASTER_GIT_BRANCH = 'refs/heads/master'
+
+  class InvalidGitBranch < RuntimeError ; end
+
+  rescue_from InvalidGitBranch do
+    # Render a HTTP 200 even though it's invalid so we don't annoy Github
+    Rails.logger.info "Not processing Github push for '#{email}' as it's for '#{git_branch}'"
+    render text: '', status: :ok
+  end
+
+  before_filter :validate_request_source, :validate_master_branch
 
   def create
     if new_event_for_user(metric)
@@ -32,21 +42,29 @@ class Api::Adapters::GithubController < Api::AdapterController
 
   def valid_request_sources
     %w{
+      127.0.0.1/32
       207.97.227.253/32
       50.57.128.197/32
       108.171.174.178/32
       50.57.231.61/32
       204.232.175.64/27
       192.30.252.0/22
-      127.0.0.1/32
 
     }.map do |ip|
      IPAddr.new(ip).to_range
     end
   end
 
+  def validate_master_branch
+    raise InvalidGitBranch unless git_branch == MASTER_GIT_BRANCH
+  end
+
   def payload
     JSON.parse(params[:payload])
+  end
+
+  def git_branch
+    payload['ref']
   end
 
   def email
