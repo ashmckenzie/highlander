497e97bcd5d3250c529828096abdb462cf1458cb
Ash McKenzie <ash@greenworm.com.au>
2013-03-21 23:40:35 +1100

    Twitter handle now displayed in User#show

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,6 +1,14 @@
 class UserDecorator < Draper::Decorator
   delegate_all
 
+  def twitter_handle
+    if source.twitter_username
+      "@#{source.twitter_username}"
+    else
+      nil
+    end
+  end
+
   def last_event_created_at
     if source.events.first
       source.events.first.created_at.strftime("%d/%m/%Y %I:%M:%S %p")
