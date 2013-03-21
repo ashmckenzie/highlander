b83ae722fce733d6815448f4c7128c37d45c0b72
Ash McKenzie <ash@greenworm.com.au>
2013-03-21 12:25:52 +1100

    woops

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -2,8 +2,8 @@ class UserDecorator < Draper::Decorator
   delegate_all
 
   def last_event_created_at
-    if source.events.last
-      source.events.last.created_at.strftime("%d/%m/%Y %I:%M:%S %p")
+    if source.events.first
+      source.events.first.created_at.strftime("%d/%m/%Y %I:%M:%S %p")
     else
       'N/A'
     end
