f80491102c2c735f22c692f023908971c4487ae5
Stuart Liston <stuart.liston@gmail.com>
2013-03-21 21:29:36 +1100

    Wee cheeky scope addition on-the-fly. Allowing us to access events of a particular metric without creating the scope explicitly

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,8 @@ class Event < ActiveRecord::Base
 
   validate :value, numericality: true
 
+  Metric.all.collect(&:name).each do |metric|
+    scope metric.pluralize.to_sym, -> { joins(:metric).where("metrics.name = '#{metric}'") }
+  end
+
 end
