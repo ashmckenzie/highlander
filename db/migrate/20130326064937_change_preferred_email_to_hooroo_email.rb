3116d7510a3019123550d7662917c9213faef247
Stuart Liston <stuart.liston@gmail.com>
2013-03-26 18:29:51 +1100

    Find and replace fail

--- db/migrate/20130326064937_change_preferred_email_to_hooroo_email.rb
+++ db/migrate/20130326064937_change_preferred_email_to_hooroo_email.rb
@@ -1,5 +1,5 @@
 class ChangePreferredEmailToHoorooEmail < ActiveRecord::Migration
   def change
-    rename_column :users, :hooroo_email, :hooroo_email
+    rename_column :users, :preferred_email, :hooroo_email
   end
 end
