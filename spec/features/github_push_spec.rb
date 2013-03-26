021d24b7e0d58cad8ca774784f51aac68be69cf6
Stuart Liston <stuart.liston@gmail.com>
2013-03-26 16:05:49 +1100

    Adding a second test case in for github pushes

--- spec/features/github_push_spec.rb
+++ spec/features/github_push_spec.rb
@@ -1,13 +1,10 @@
 require "spec_helper"
 
-feature "First Github push" do
+feature 'Github Push' do
 
   background do
-
     @first_time_badge       = FactoryGirl.create(:first_time)
     @one_github_push_badge  = FactoryGirl.create(:one_github_push)
-
-    page.driver.post api_adapters_github_index_path, valid_params
   end
 
   given(:valid_params) do
@@ -20,12 +17,32 @@ feature "First Github push" do
   given(:user)    { FactoryGirl.create(:user) }
   given(:metric)  { FactoryGirl.create(:github_push) }
 
-  scenario "User is given the First Github push and First Timer badges" do
-    visit user_path(user)
-    page.should have_content @first_time_badge.description
-    page.should have_content @one_github_push_badge.description
+  describe "First push" do
 
-    page.should have_content "#{metric.default_unit} Total Score"
-    page.should have_content "2 Badges"
+    background { page.driver.post api_adapters_github_index_path, valid_params }
+
+    scenario "User is given the First Github push and First Timer badges" do
+      visit user_path(user)
+      page.should have_content @first_time_badge.description
+      page.should have_content @one_github_push_badge.description
+
+      page.should have_content "#{metric.default_unit} Total Score"
+      page.should have_content "2 Badges"
+    end
   end
+
+  describe "Second push" do
+
+    background { 2.times { page.driver.post api_adapters_github_index_path, valid_params } }
+
+    scenario "User has more points" do
+      visit user_path(user)
+      page.should have_content @first_time_badge.description
+      page.should have_content @one_github_push_badge.description
+
+      page.should have_content "#{metric.default_unit * 2} Total Score"
+      page.should have_content "2 Badges"
+    end
+  end
+
 end
