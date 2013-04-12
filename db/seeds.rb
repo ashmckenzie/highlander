c3393146362a573484120d18a36fb5dbf7553ce3
Ash McKenzie <ash@greenworm.com.au>
2013-04-12 14:19:39 +1000

    Random event creation now limited to three metrics

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,16 @@
 raise 'Only for development yo!' if ENV['RAILS_ENV'] == 'production'
 
 User.all.each do |user|
-  Metric.all.each do |metric|
+  Metric.all.shuffle.take(3).each do |metric|
     count = rand(50)+1
+
     message = "Creating #{count} #{metric.name} events for #{user.name}"
     puts message
     Rails.logger.info message
+
     1.upto(count).each do
-      user.events << Event.new(metric: metric)
+      created_at = Time.zone.now - (rand(21)+1).days
+      user.events << Event.new(metric: metric, created_at: created_at)
     end
   end
 end
