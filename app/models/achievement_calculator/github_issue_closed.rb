cda77cd3d80e28e7a66d822d482af1de72610d7a
Stuart Liston <stuart.liston@gmail.com>
2013-04-13 18:24:33 +1000

    Fixed calculators for github issues

--- app/models/achievement_calculator/github_issue_closed.rb
+++ app/models/achievement_calculator/github_issue_closed.rb
@@ -6,7 +6,7 @@ module AchievementCalculator
 
     def badges
       {
-        1   => '1_github_issues_closed',
+        1   => '1_github_issue_closed',
         25  => '25_github_issues_closed',
         50  => '50_github_issues_closed',
         100 => '100_github_issues_closed',
--- app/models/achievement_calculator/github_issue_opened.rb
+++ app/models/achievement_calculator/github_issue_opened.rb
@@ -6,7 +6,7 @@ module AchievementCalculator
 
     def badges
       {
-        1   => '1_github_issues_opened',
+        1   => '1_github_issue_opened',
         25  => '25_github_issues_opened',
         50  => '50_github_issues_opened',
         100 => '100_github_issues_opened',
