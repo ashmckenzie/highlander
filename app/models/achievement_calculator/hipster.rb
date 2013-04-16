39fcc841be45fac4235bb81363fcd5527bd61431
Stuart Liston <stuart.liston@gmail.com>
2013-04-16 21:40:53 +1000

    User can now only earn 1 hipster badge #2

--- app/models/achievement_calculator/hipster.rb
+++ app/models/achievement_calculator/hipster.rb
@@ -7,8 +7,9 @@ module AchievementCalculator
     end
 
     def calculate!
-      return if achievement_is_hipster_badge?
+      return if achievement_is_invalid_for_hipster_badge?
       return if achievement_is_mainstream?
+      return if user_already_hipster?
       hipster!
     end
 
@@ -32,12 +33,16 @@ module AchievementCalculator
       @hipster_badge ||= Badge.find_by_name('hipster')
     end
 
-    def achievement_is_hipster_badge?
+    def achievement_is_invalid_for_hipster_badge?
       achievement.badge_name == hipster_badge.name
     end
 
     def achievement_is_mainstream?
       achievement.users.count > 1
+    end
+
+    def user_already_hipster?
+      Achievement.where(user: user, badge: hipster_badge).count > 0
     end
 
   end
--- spec/features/hipster_spec.rb
+++ spec/features/hipster_spec.rb
@@ -10,8 +10,9 @@ feature 'Hipster Badge' do
     @first_jenkins_green_job_badge  = FactoryGirl.create(:one_jenkins_green_job)
   end
 
-  given(:user)    { FactoryGirl.create(:user) }
-  given(:metric)  { FactoryGirl.create(:jenkins_green_job) }
+  given(:user)          { FactoryGirl.create(:user) }
+  given(:existing_user) { FactoryGirl.create(:user, name: 'Jane Smith') }
+  given(:metric)        { FactoryGirl.create(:jenkins_green_job) }
 
   given(:params) do
     { metric: metric.name, email:  user.hooroo_email }
@@ -21,14 +22,30 @@ feature 'Hipster Badge' do
 
     background { page.driver.post endpoint, params }
 
-    scenario "User is given the First Github push, First-time and Hipster badges" do
+    scenario "User is given the First Github push, First-time and ONE Hipster badge" do
       visit user_path(user)
-      page.should have_content '4 Badges'
+      page.should have_content '3 Badges'
       page.should have_content @first_jenkins_green_job_badge.description
-      page.should have_content @hipster_badge.description
+      page.should have_content @hipster_badge.tag
       page.should have_content "#{metric.default_unit} All-time"
       page.should have_content "You got the #{@first_time_badge.description} badge before it was cool"
-      page.should have_content "You got the #{@first_jenkins_green_job_badge.description} badge before it was cool"
+      page.should_not have_content "You got the #{@first_jenkins_green_job_badge.description} badge before it was cool"
+    end
+  end
+
+  describe "First green job - when another user has the badges already" do
+
+    background do
+      page.driver.post endpoint, params.merge(email: existing_user.hooroo_email)
+      page.driver.post endpoint, params.merge(email: user.hooroo_email)
+    end
+
+    scenario "User is given the First Github push, First-time and NO Hipster badges" do
+      visit user_path(user)
+      page.should have_content '2 Badges'
+      page.should have_content @first_jenkins_green_job_badge.description
+      page.should have_content "#{metric.default_unit} All-time"
+      page.should_not have_content @hipster_badge.tag
     end
   end
 
