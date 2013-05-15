d1fcf677ea8413d71540438344058e70351dd7fc
Ash McKenzie <ash@greenworm.com.au>
2013-05-15 23:46:56 +1000

    Some users has an empty github_username

--- db/data_migrations/20130515151403_convert_github_to_github_service.rb
+++ db/data_migrations/20130515151403_convert_github_to_github_service.rb
@@ -3,7 +3,7 @@ module DataMigration
     class ConvertGithubToGithubService < Base
 
       def up
-        User.where('github_username is NOT NULL').each do |user|
+        User.where("github_username is NOT NULL AND github_username != ''").each do |user|
           github_service = Services::Github.new(username: user.github_username, emails: user.emails)
           user.user_services << UserService.new(service: github_service)
         end
