0245ad368776de7afcfb9429b137ca003c93b544
Ash McKenzie <ash@greenworm.com.au>
2013-05-31 12:34:47 +1000

    More bounty validations

--- app/models/bounty.rb
+++ app/models/bounty.rb
@@ -4,10 +4,15 @@ class Bounty < ActiveRecord::Base
   belongs_to :claimed_by, class_name: 'User', foreign_key: 'claimed_by_id'
 
   validate :ensure_not_claimed
+  validate :ensure_max_active_bounties
 
   validates :name, :description, :reward, presence: true
   validates :name, uniqueness: true
   validates :reward, inclusion: { in: 5..10 , message: "must be between 5 and 10" }
+
+  scope :unclaimed, -> { where("claimed_by_id IS NULL AND claimed_at IS NULL") }
+
+  MAX_ACTIVE_BOUNTIES = 2
 
   def claimed?
     claimed_by && claimed_at ? true : false
@@ -16,6 +21,10 @@ class Bounty < ActiveRecord::Base
   private
 
   def ensure_not_claimed
-    errors.add(:claimed_by, "already claimed by #{claimed_by.name}") if (attributes['claimed_by'] && attributes['claimed_at'])
+    errors.add(:claimed_by, "Already claimed by #{claimed_by.name}") if (attributes['claimed_by'] && attributes['claimed_at'])
+  end
+
+  def ensure_max_active_bounties
+    errors.add(:id, "You can only have #{self.class::MAX_ACTIVE_BOUNTIES} bounties at any one time") if self.class.unclaimed.where("created_by_id = #{self.created_by_id}").count > self.class::MAX_ACTIVE_BOUNTIES
   end
 end
