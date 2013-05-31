cda218c4a1e29ed79843909ef9705a6491281399
Ash McKenzie <ash@greenworm.com.au>
2013-05-31 11:34:59 +1000

    Disable better logging.. for now.

--- config/initializers/00_better_logging.rb
+++ config/initializers/00_better_logging.rb
@@ -1,7 +1,9 @@
-class ActiveSupport::BufferedLogger
-  def formatter=(formatter)
-    @log.formatter = formatter
-  end
-end
+# Disabled for now as it's causing issues with Rails 4 :(
+#
+# class ActiveSupport::BufferedLogger
+#   def formatter=(formatter)
+#     @log.formatter = formatter
+#   end
+# end
 
-Rails.logger.formatter = BetterLogger.new
+# Rails.logger.formatter = BetterLogger.new
