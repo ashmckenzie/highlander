63bff6098c67fb47ff2792396e10d714d073cd38
Ash McKenzie <ash@greenworm.com.au>
2013-06-03 09:45:10 +1000

    Added missing 100_ming_pong_victories badge (sorry Phil!)

--- db/data_migrations/20130228032129_add_badges.rb
+++ db/data_migrations/20130228032129_add_badges.rb
@@ -20,6 +20,7 @@ module DataMigration
         Badge.create!(name: '1_ming_pong_victory', tag: 'One small step forward', description: 'First Ming Pong victory')
         Badge.create!(name: '1_ming_pong_loss', tag: "Well that's to be expected", description: 'First Ming Pong loss')
         Badge.create!(name: '10_ming_pong_victories', tag: 'Paddle master', description: '10 Ming Pong victories')
+        Badge.create!(name: '100_ming_pong_victories', tag: 'Paddle master', description: '100 Ming Pong victories')
 
         Badge.create!(name: '1_twitter_mention', tag: 'Fly fly little birdie!', description: 'First Hooroo Twitter mention')
         Badge.create!(name: '25_twitter_mentions', tag: 'In a flutter', description: '25 Hooroo Twitter mentions')
