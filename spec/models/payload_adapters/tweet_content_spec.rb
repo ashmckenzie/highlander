28be0832bbe42e2431d5e3f0e377750b20ce3f78
Ash McKenzie <ash@greenworm.com.au>
2013-06-13 14:22:32 +1000

    Spec fix for newly namespaced TweetContent class

--- spec/models/payload_adapters/tweet_content_spec.rb
+++ spec/models/payload_adapters/tweet_content_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
-describe TweetContent do
+describe PayloadAdapters::TweetContent do
 
   describe '#to_s' do
 
     it 'is the raw tweet content/text' do
       tweet_content = 'This is a really important tweet'
-      TweetContent.new(tweet_content).to_s.should == tweet_content
+      described_class.new(tweet_content).to_s.should == tweet_content
     end
   end
 
@@ -16,7 +16,7 @@ describe TweetContent do
       let(:tweet_content) { 'Please visit hooroo.com, it is cool.' }
 
       it 'is true' do
-        TweetContent.new(tweet_content).high_value?.should be_true
+        described_class.new(tweet_content).high_value?.should be_true
       end
     end
 
@@ -24,7 +24,7 @@ describe TweetContent do
       let(:tweet_content) { 'Please visit hooroooo.com, it is cool.' }
 
       it 'is false' do
-        TweetContent.new(tweet_content).high_value?.should be_false
+        described_class.new(tweet_content).high_value?.should be_false
       end
     end
 
@@ -32,7 +32,7 @@ describe TweetContent do
       let(:tweet_content) { 'Please visit goo.gl/W6tPN, it is cool. Or visit http://bit.ly/L5ePra' }
 
       it 'is true' do
-        TweetContent.new(tweet_content).high_value?.should be_true
+        described_class.new(tweet_content).high_value?.should be_true
       end
     end
 
