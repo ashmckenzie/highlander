009b2b4919bcee39319d75da26d5673c67904ea7
Ash McKenzie <ash@greenworm.com.au>
2013-06-13 18:56:40 +1000

    Double time zone conversion :(

--- config/initializers/stethoscope.rb
+++ config/initializers/stethoscope.rb
@@ -2,5 +2,5 @@ Stethoscope.check :events, :critical do |response|
   query = 'SELECT created_at FROM events order by created_at DESC LIMIT 1'
   result = ActiveRecord::Base.connection.execute(query)
   response['query'] = query.inspect
-  response['most_recent'] = Time.zone.parse(result.first['created_at'])
+  response['most_recent'] = Time.parse(result.first['created_at'])
 end
