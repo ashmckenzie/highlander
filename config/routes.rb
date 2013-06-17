c14e77e313cd048ff642cd05bff8cbc7da491661
Ash McKenzie <ash@greenworm.com.au>
2013-06-17 23:18:28 +1000

    Disabled /about page for now

--- config/routes.rb
+++ config/routes.rb
@@ -31,17 +31,26 @@ Highlander::Application.routes.draw do
   get   '/signout' => 'sessions#destroy', as: :signout
   post  '/auth/google_apps/callback' => 'sessions#create'
 
+  # NOTE: Disabled for now.  Big question is, in what context does it belong?
+  #
+  # get '/about' => 'high_voltage/pages#show', id: 'about'
+
+  # Rounds under this constraint are for Organisations
+  #
+  # eg. Hooroo, Agile Aus
+  #
   constraints(:subdomain => /^(?!www).+$/) do
 
     resources :users
     resources :badges, only: [ :index, :show ]
     resources :bounties
 
-    get '/about' => 'high_voltage/pages#show', id: 'about'
-
     root to: 'welcome#index'
   end
 
+  # Routes under this constraint are when no Organisation has been defined,
+  # or it's www
+  #
   constraints(:subdomain => /.*/) do
 
     resources :registrations, only: [ :index, :create ]
