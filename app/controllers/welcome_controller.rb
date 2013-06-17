079a44218793d579e62c0837d2f0746de4c96bd1
Ash McKenzie <ash@greenworm.com.au>
2013-06-17 22:08:01 +1000

    Cache welcome#index for 5 mins

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
 
   def index
-    @users = Queries::RunningLeaderboard.new.query.decorate
+    Rails.cache.fetch('leaderboard', expires_in: 5.minutes) do
+      @users = Queries::RunningLeaderboard.new.query.decorate
+    end
   end
 end
