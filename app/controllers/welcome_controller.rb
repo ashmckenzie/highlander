2507fe91292bfb7c356f7005c26823dfb0fb78e6
Ash McKenzie <ash@greenworm.com.au>
2013-06-17 22:44:46 +1000

    Am I really that stupid?

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,8 @@
 class WelcomeController < ApplicationController
 
   def index
-    Rails.cache.fetch('leaderboard', expires_in: 5.minutes) do
-      @users = Queries::RunningLeaderboard.new.query.decorate
+    @users = Rails.cache.fetch('leaderboard', expires_in: 5.minutes) do
+      Queries::RunningLeaderboard.new.query.decorate
     end
   end
 end
