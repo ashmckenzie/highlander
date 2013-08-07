545aa143f3591b5e3f496ed8728b9667ea4a818b
Ash McKenzie <ash@greenworm.com.au>
2013-08-07 18:25:35 +1000

    Joined Events graphs together

--- app/models/queries/stats.rb
+++ app/models/queries/stats.rb
@@ -18,19 +18,27 @@ module Queries
       event_results = {}
       achievement_results = {}
 
+      point_in_time = Time.at(0) #(Time.zone.now - 3.months)
+
       if events
+        events_three_months_recent = "events.created_at > '#{point_in_time}'"
+
         event_results = {
-          events_by_created_at: Event.joins(:user).where(conditions).group_by_day('events.created_at').count.sort,
           events_by_metric: Event.joins(:metric, :user).where(conditions).group('metrics.description').count.sort { |x, y| y[1] <=> x[1] },
-          github_pushes_by_created_at: Event.joins(:metric, :user).where(conditions).where("metrics.name = 'github_push'").group_by_day("events.created_at").count.sort,
-          jenkins_green_jobs_by_created_at: Event.joins(:metric, :user).where(conditions).where("metrics.name = 'jenkins_green_job'").group_by_day("events.created_at").count.sort
+          events: {
+            all: Event.joins(:user).where(conditions).where(events_three_months_recent).group_by_day('events.created_at').count.sort,
+            github: Event.joins(:metric, :user).where(conditions).where(events_three_months_recent).where("metrics.name = 'github_push'").group_by_day("events.created_at").count.sort,
+            jenkins: Event.joins(:metric, :user).where(conditions).where(events_three_months_recent).where("metrics.name = 'jenkins_green_job'").group_by_day("events.created_at").count.sort
+          }
         }
       end
 
       if achievements
+        achievements_three_months_recent = "achievements.created_at > '#{point_in_time}'"
+
         achievement_results = {
-          achievements_by_created_at: Achievement.unscoped.joins(:user).where(conditions).group_by_day('achievements.created_at').count.sort,
-          achievements_by_badge: Achievement.unscoped.joins(:badge, :user).where(conditions).group('badges.description').count.sort { |x, y| y[1] <=> x[1] }
+          achievements_by_created_at: Achievement.unscoped.joins(:user).where(conditions).where(achievements_three_months_recent).group_by_day('achievements.created_at').count.sort,
+          achievements_by_badge: Achievement.unscoped.joins(:badge, :user).where(conditions).where(achievements_three_months_recent).group('badges.description').count.sort { |x, y| y[1] <=> x[1] }
         }
       end
 
