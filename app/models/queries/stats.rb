136db6b90883edc0afa802330ed5040f05416c57
Ash McKenzie <ash@greenworm.com.au>
2013-08-07 17:54:54 +1000

    Fixed sort on stats (events)

--- app/models/queries/stats.rb
+++ app/models/queries/stats.rb
@@ -20,17 +20,17 @@ module Queries
 
       if events
         event_results = {
-          events_by_created_at: Event.joins(:user).where(conditions).group_by_day('events.created_at').count,
+          events_by_created_at: Event.joins(:user).where(conditions).group_by_day('events.created_at').count.sort,
           events_by_metric: Event.joins(:metric, :user).where(conditions).group('metrics.description').count.sort { |x, y| y[1] <=> x[1] },
-          github_pushes_by_created_at: Event.joins(:metric, :user).where(conditions).where("metrics.name = 'github_push'").group_by_day("events.created_at").count,
-          jenkins_green_jobs_by_created_at: Event.joins(:metric, :user).where(conditions).where("metrics.name = 'jenkins_green_job'").group_by_day("events.created_at").count,
+          github_pushes_by_created_at: Event.joins(:metric, :user).where(conditions).where("metrics.name = 'github_push'").group_by_day("events.created_at").count.sort,
+          jenkins_green_jobs_by_created_at: Event.joins(:metric, :user).where(conditions).where("metrics.name = 'jenkins_green_job'").group_by_day("events.created_at").count.sort
         }
       end
 
       if achievements
         achievement_results = {
-          achievements_by_created_at: Achievement.unscoped.joins(:user).where(conditions).group_by_day('achievements.created_at').count,
-          achievements_by_badge: Achievement.unscoped.joins(:badge, :user).where(conditions).group('badges.description').count.sort { |x, y| y[1] <=> x[1] },
+          achievements_by_created_at: Achievement.unscoped.joins(:user).where(conditions).group_by_day('achievements.created_at').count.sort,
+          achievements_by_badge: Achievement.unscoped.joins(:badge, :user).where(conditions).group('badges.description').count.sort { |x, y| y[1] <=> x[1] }
         }
       end
 
