0bf3ac1dbf2cfb683c1903f276f383e59d81145e
Ash McKenzie <ash@greenworm.com.au>
2013-08-28 16:51:34 +1000

    Fixed Bounty created/claimed at times only displaying created_at (derp)

--- app/decorators/bounty_decorator.rb
+++ app/decorators/bounty_decorator.rb
@@ -2,17 +2,16 @@ class BountyDecorator < Draper::Decorator
   delegate_all
 
   def claimed_at
-    better_time object
+    better_time :claimed_at
   end
 
   def created_at
-    better_time object
+    better_time :created_at
   end
 
   private
 
-  def better_time object
-    object.created_at.strftime("%m/%d/%y %H:%M")
-  end
-
+    def better_time time_field
+      object.public_send(time_field).strftime("%m/%d/%y %H:%M")
+    end
 end
