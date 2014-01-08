def1221f5fb05052c4c52631b8fe620caf2e79a8
Ash McKenzie <ash@greenworm.com.au>
2014-01-08 20:47:07 +1100

    Slight refactor to Api::EventsController

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -7,26 +7,28 @@ module Api
     before_filter :validate_payload!
 
     def create
-      if Event.create(payload.to_event_hash)
-        code = :ok
-      else
-        code = :not_found
-      end
-
       respond_to do |format|
-        format.json { head code }
+        format.json { head create_event! }
       end
     end
 
     private
 
-    def validate_payload!
-      payload.validate!
-    end
+      def create_event!
+        if Event.create(payload.to_event_hash)
+          :ok
+        else
+          :not_found
+        end
+      end
 
-    def payload
-      @payload ||= Factories::PayloadAdapterFactory.for(params)
-    end
+      def validate_payload!
+        payload.validate!
+      end
+
+      def payload
+        @payload ||= Factories::PayloadAdapterFactory.for(params)
+      end
 
   end
 
