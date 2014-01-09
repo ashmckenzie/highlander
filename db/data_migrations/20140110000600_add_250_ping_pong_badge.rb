58f91bf45309329b523ab6923b5b7ab8b4ea8b6e
Ash McKenzie <ash@greenworm.com.au>
2014-01-10 00:25:54 +1100

    Argh, I'm hopeless

--- db/data_migrations/20140110000600_add_250_ping_pong_badge.rb
+++ db/data_migrations/20140110000600_add_250_ping_pong_badge.rb
@@ -3,11 +3,12 @@ module DataMigration
     class Add250PingPongBadge < Base
 
       def up
-        Badge.create!(name: '250_ming_pong_victories', tag: 'Paddle guru', description: '250 Ping Pong victories')
+        Badge.create!(name: '250_ping_pong_victories', tag: 'Paddle guru', description: '250 Ping Pong victories')
       end
 
       def down
         Badge.where(name: '250_ming_pong_victories').destroy_all
+        Badge.where(name: '250_ping_pong_victories').destroy_all
       end
     end
   end
