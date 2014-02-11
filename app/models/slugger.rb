94acc6b74240c519971c01232daeea538688033c
Ash McKenzie <ash@greenworm.com.au>
2014-02-11 16:56:44 +1100

    Slugger is now sets the slug on before_create

--- app/models/slugger.rb
+++ app/models/slugger.rb
@@ -9,15 +9,18 @@ module Slugger
 
     validates :slug, uniqueness: true, presence: true, on: :create
 
-    before_validation :set_slug, on: :create
+    before_create :set_slug
 
     def to_param
       slug
     end
 
-    def set_slug
-      new_slug = name.downcase.gsub(/[^a-z0-9\.\-\s]/i, '').gsub(/[\s+\.]/, '-')
-      self.slug = new_slug
+    def set_slug slug=default_slug
+      self.slug = slug
+    end
+
+    def default_slug
+      name.downcase.gsub(/[^a-z0-9\.\-\s]/i, '').gsub(/[\s+\.]/, '-')
     end
   end
 
