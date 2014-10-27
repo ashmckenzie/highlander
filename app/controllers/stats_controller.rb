7d7ea3ff5ac4c5c113ca331f133da61070b4a46a
Ash McKenzie <ash@the-rebellion.net>
2014-10-28 08:46:30 +1100

    More /stats fixes

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -2,7 +2,10 @@ class StatsController < ApplicationController
 
   def index
     @stats = Rails.cache.fetch('stats', expires_in: 30.minutes) do
-      Queries::Stats.new.query
+      Hashie::Mash.new({
+        all_time:    Queries::Stats.new.query,
+        past_month:  Queries::Stats.new.query(Time.zone.now - 1.month)
+      })
     end
   end
 end
